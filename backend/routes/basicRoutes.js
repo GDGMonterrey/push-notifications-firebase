5ecb2df47bcc81106b591872d16eeee06900c241
DezkaReid <elmaildeldezkareid@gmail.com>
2017-02-12 04:09:43 -0600

    Removed logs

--- backend/routes/basicRoutes.js
+++ backend/routes/basicRoutes.js
@@ -11,28 +11,6 @@ basicRoutes.get('/', function(req, res) {
 	res.json({ version : "1.0.0" });
 });
 
-let payload = {
-	data : {
-		pendientes: "Al chile no quiero hacerlo"
-	}
-}
-/*let token = "fqV7KdbFHtw:APA91bFFnCT7tNSMl-zYLQ6kfxqQG8iAe2SNDN1kpbVdcjaVl5hSU2bkfYrYTxp3ls8zCOPgVcjURyWDGTv65uMjKmDIDJwNWd4C2xtaEJntHEWNyYugLwaBFXJXMH_AsRhVq3BI9V9I";
-notificationService.linkToTopicRequest(token,"nodejs")
-	.then(console.log)
-	.catch((error)=>{
-		console.log(error);
-		console.log(error[0]);
-	});
-/**/
-let topic = "nodejs";
-firebaseService.sendNotificationToTopic(topic,payload)
-	.then((response)=>{
-		console.log(response);
-	})
-	.catch((error)=>{
-		console.log(error);
-		console.log(error[0]);
-	});
 basicRoutes.post('/register', (req, res) => {
 	if (req.body && req.body.token && req.body.topic ){
 		notificationService.linkToTopicRequest(req.body.token, req.body.topic)
@@ -78,7 +56,7 @@ basicRoutes.post('/topic-message', (req, res) => {
 basicRoutes.post('/multi-message', function(req, res) {
 	if (req.body && req.body.topics && req.body.payload ){
 		let condition = firebaseService.createCondition(req.body.topics);
-		firebaseService.sendToCondition(condition, payload)(req.body.topic, req.body.payload)
+		firebaseService.sendToCondition(condition, req.body.payload)
 			.then((response)=>{
 				res.json(response);
 			})
