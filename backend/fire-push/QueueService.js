47a3f3866c34a208ca73176389834497ed515de6
DezkaReid <elmaildeldezkareid@gmail.com>
2017-03-02 23:09:58 -0600

    Added queue using KUE

--- backend/fire-push/QueueService.js
+++ backend/fire-push/QueueService.js
@@ -1,23 +1,37 @@
 import * as amq_api from 'amqplib/callback_api';
+import kue from 'kue';
+const queue = kue.createQueue();
 
 const sendMessageRabbitMQ = function (data) {
 	amq_api.connect('amqp://localhost', function(err, conn) {
 		conn.createChannel(function(err, channel) {
-		let queue = 'firebase_queue';
-		let configQueue = {
-			durable: false,
-			timestamp: Date.now(),
-			contentEncoding: 'utf-8'
-		};
-		channel.assertQueue(queue, configQueue);
-		channel.sendToQueue(queue, Buffer.from(JSON.stringify(data)));
-		console.log("Message sended");
+			let queue = 'firebase_queue';
+			let configQueue = {
+				durable: false,
+				timestamp: Date.now(),
+				contentEncoding: 'utf-8'
+			};
+			channel.assertQueue(queue, configQueue);
+			channel.sendToQueue(queue, Buffer.from(JSON.stringify(data)));
+			console.log("Message sended");
+		});
 	});
-});
+}
+
+const sendMessageKUE = function (data) {
+	queue
+		.create('firebase_queue', data)
+		.priority('normal')
+		.attempts(5)
+		.save();
 }
 
 export default {
 	sendMessage : function(data){
-		sendMessageRabbitMQ(data);
+		if (process.env.use_rabbit){
+			sendMessageRabbitMQ(data);
+		} else {
+			sendMessageKUE(data);
+		}
 	}
 }
